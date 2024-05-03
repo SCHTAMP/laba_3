b1cd4e4207d7069da451feeee194c910a0ec8bc1
SCHTAMP <42341716+SCHTAMP@users.noreply.github.com>
2024-05-03 11:46:48 +0300

    Update 1_3_env_vars.sh

--- 1_3_env_vars.sh
+++ 1_3_env_vars.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# Получаем все переменные окружения
 exported_variables=$(set | tr '\n' ' ')
-
-# Удаляем пробелы в начале и конце каждой переменной
 trimmed_variables=${exported_variables// / }
-
-# Разделяем переменные по пробелам и удаляем лишние пробелы
 IFS=' ' read -ra variables <<< "$trimmed_variables"
-
-# Сортируем массив переменных
 sorted_variables=( $(echo "${variables[@]}" | sort) )
 
-# Выводим отсортированный список переменных
 for var in "${sorted_variables[@]}"; do
     echo "$var"
 done
