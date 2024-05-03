98a02a1068f5497249451bb77dbc8fb211032b73
SCHTAMP <42341716+SCHTAMP@users.noreply.github.com>
2024-05-03 11:52:29 +0300

    Update 2_2_1.sh

--- 2_2_1.sh
+++ 2_2_1.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# Получаем текущую дату и время
 current_time=$(date +"%Y-%m-%d %H:%M:%S")
-
-# Добавляем строку с датой и временем в конец журнала
 echo "$current_time" >> /tmp/run.log
-
-# Выводим "Hello, World!" в стандартный вывод
 echo "Hello, World!"
-
-# Подсчитываем количество строк в журнале
 lines_count=$(wc -l < /tmp/run.log)
-
-# Выводим количество строк в стандартный вывод ошибок
 echo "$lines_count" >&2
 
 less /tmp/run.log
