2b7d39ee27f9b6e35d3103251d1c355008355dc8
SCHTAMP <42341716+SCHTAMP@users.noreply.github.com>
2024-05-03 11:48:59 +0300

    Update 1_3_env_vars.sh

--- 1_3_env_vars.sh
+++ 1_3_env_vars.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-exported_variables=$(set | tr '\n' ' ')
-trimmed_variables=${exported_variables// / }
-IFS=' ' read -ra variables <<< "$trimmed_variables"
-sorted_variables=( $(echo "${variables[@]}" | sort) )
-
-for var in "${sorted_variables[@]}"; do
-    echo "$var"
-done
+env | sort
