0b882982e3d3a9ae58139df4f09b9027915dc37e
Klishin K.S <KlishinKS@mpei.ru>
2024-05-03 04:27:28 -0400

    3.4 Перепишите задание 1.11. так, чтобы исследуемый каталог  передавался через аргумент командной  строки.

---
+++ 3_4.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+directory=$1
+
+if [[ -z "$directory" ]]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+cd "$directory" || exit 1
+
+function print_directories() {
+    find . -type d -print0 | xargs -0 ls -ld --time-style=long-iso
+}
+
+function print_regular_files() {
+    find . -type f -print0 | xargs -0 ls -l --time-style=long-iso
+}
+
+function print_symbolic_links() {
+    find -L . -xtype l -print0 | xargs -0 ls -l --time-style=long-iso
+}
+
+function print_character_devices() {
+    find . -type c -print0 | xargs -0 ls -l --time-style=long-iso
+}
+
+function print_block_devices() {
+    find . -type b -print0 | xargs -0 ls -l --time-style=long-iso
+}
+
+echo "Каталоги:"
+print_directories
+echo "Обычные файлы:"
+print_regular_files
+echo "Символьные ссылки:"
+print_symbolic_links
+echo "Символьные устройства:"
+print_character_devices
+echo "Блочные устройства:"
+print_block_devices
