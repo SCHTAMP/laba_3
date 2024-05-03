7a50441a9ded657d654c272cb0df62fff0098a8d
SCHTAMP <42341716+SCHTAMP@users.noreply.github.com>
2024-05-03 11:46:26 +0300

    Update 1_11.sh

--- 1_11.sh
+++ 1_11.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
-# Функция для вывода информации о каталогах
 function print_directories() {
     find . -type d -print0 | xargs -0 ls -ld --time-style=long-iso
 }
 
-# Функция для вывода информации об обычных файлах
 function print_regular_files() {
     find . -type f -print0 | xargs -0 ls -l --time-style=long-iso
 }
 
-# Функция для вывода информации о символьных ссылках
 function print_symbolic_links() {
     find . -type l -print0 | xargs -0 ls -l --time-style=long-iso
 }
 
-# Функция для вывода информации о символьных устройствах
 function print_character_devices() {
     find . -type c -print0 | xargs -0 ls -l --time-style=long-iso
 }
 
-# Функция для вывода информации о блочных устройствах
 function print_block_devices() {
     find . -type b -print0 | xargs -0 ls -l --time-style=long-iso
 }
 
-# Вызываем функции в нужном порядке
 echo "Каталоги:"
 print_directories
 echo "Обычные файлы:"
