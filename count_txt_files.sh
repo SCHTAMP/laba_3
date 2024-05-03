11d66ada238fc4f54da404af127434f90a7b81f7
SCHTAMP <42341716+SCHTAMP@users.noreply.github.com>
2024-05-03 11:45:47 +0300

    Update count_txt_files.sh

--- count_txt_files.sh
+++ count_txt_files.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Функция для подсчета количества файлов с расширением .txt
 function count_txt_files() {
     local count=0
     for file in "$HOME"/*; do
@@ -11,5 +10,4 @@ function count_txt_files() {
     echo "$count"
 }
 
-# Вызов функции и вывод результата
 echo "Количество файлов .txt в домашнем каталоге: $(count_txt_files)"
