4f4ae1b1eb864526fc7eba62d7afbee4cd93d55c
SCHTAMP <42341716+SCHTAMP@users.noreply.github.com>
2024-05-03 11:49:26 +0300

    Update 1_4_good_morning.sh

--- 1_4_good_morning.sh
+++ 1_4_good_morning.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Функция для вывода текущего времени и календаря на текущий месяц
+user="$(whoami)"
+
 function print_time_and_calendar() {
     date "+%H:%M %A, %B %d, %Y"
    (cal -C)
 }
 
-# Функция для вывода списка дел из файла TODO
 function print_todo_list() {
     if [ -f "$HOME/TODO" ]; then
         cat "$HOME/TODO"
@@ -15,13 +15,6 @@ function print_todo_list() {
     fi
 }
 
-user="$(whoami)"
-
-# Пожелание доброго утра
 echo "Доброе утро, $user!"
-
-# Вывод текущего времени и календаря
 print_time_and_calendar
-
-# Вывод списка дел из файла TODO
 print_todo_list
