0b8d9343b6fc9be3ac71cc66107c86a4e1b869e7
SCHTAMP <42341716+SCHTAMP@users.noreply.github.com>
2024-05-03 11:52:42 +0300

    Update 2_3.sh

--- 2_3.sh
+++ 2_3.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Путь к исходному файлу
 INPUT_FILE="./bash.txt"
 
-
-# Используем grep для фильтрации строк и сохранения их в соответствующие файлы
 grep -E '0{6}' "$INPUT_FILE" > /tmp/zeros
 grep -v -E '0{6}' "$INPUT_FILE" > /tmp/nozeros
 
-# Выводим первые и последние 10 строк из каждого файла
 echo "Zeros: "
 head -n 10 /tmp/zeros
 echo ""
